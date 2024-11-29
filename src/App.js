404ecfd15509944bf837e1f8ed164363d9894b75
Monika11-dev <monika.chauhan@celestialsys.com>
2024-11-29 17:26:58 +0530

    API Call

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useDispatch,useSelector } from 'react-redux';
+import {fetchTodos} from './Redux/Slice/todoslice';
+
 
 function App() {
+
+  const dispatch = useDispatch();
+  const jsonData = useSelector((state) => state.todo.data);
+
+  console.log(jsonData);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <button onClick = { (e) => dispatch(fetchTodos()) }>Fetch Todos</button>
+       {jsonData && jsonData.map((item) => (
+          <li>{item.title}</li>
+         ))}
     </div>
   );
 }
